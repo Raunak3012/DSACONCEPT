465b9727927c1dc3eaaaa960e2c19b0b4356403c
Vivek Pareek <vpareek1308@gmail.com>
2022-10-17 16:22:20 +0530

    Added floor functionality to all codes

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -3,6 +3,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+#define ll long long
+#define nl "\n"
+
+int floor(float a) {
+    if((int)a == a) return (int)a;
+    else return (int)a;
+}
+
 // Graph class represents a directed graph
 // using adjacency list representation
 class Graph {
